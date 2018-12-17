20e94ebe64292de1becff8f3d7eb7105b87b9879
lookhua <2560636051@qq.com>
2018-12-17 23:59:41 +0800

    init

--- pages/msg/msg.js
+++ pages/msg/msg.js
@@ -4,5 +4,26 @@ const app = getApp()
 
 Page({
   data: {
+    msgList: [{
+        id: 1,
+        title: "优惠券消息",
+        content: "fwefwefwef",
+        creatTime:"2018-10-09"
+      },
+      {
+        id: 2,
+        title: "优惠券消息",
+        content: " 在做小程序的时候遇到在text标签里面的文本过长,需要限制显示长度,并且在限制了长度的后面加上省略号代表后面还有内容。 这时候设置样式",
+        creatTime: "2018-10-09"
+      },
+      {
+        id: 3,
+        title: "优惠券消息",
+        content: "fwefwefwef",
+        creatTime: "2018-10-09"
+      }
+
+    ]
   }
+
 })
